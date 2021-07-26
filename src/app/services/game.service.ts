1bbfa44d3f81a448af2ba1f7df625ebd4457b3d6
MaryKattyVO <mkvicunaore@gmail.com>
2021-07-26 10:02:18 -0500

    Mostrar data en HTML y mostrar data desde caché e internet

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -2,14 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Game } from '../interfaces/interfaces';
+import { of } from 'rxjs';
+import { tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class GameService {
-
+  private juegos: Game[] = [];
   constructor( private http: HttpClient) { }
 
   getNominados() {
-    return this.http.get<Game[]>(`${environment.url}/api/goty`)
+    //Si no tenemos juegos
+    if( this.juegos.length > 0) {
+      console.log('Desde caché');
+      return of(this.juegos);
+
+    }else {
+      console.log('Desde internet');
+      return this.http.get<Game[]>(`${environment.url}/api/goty`)
+        .pipe(
+          tap(
+            juegos => this.juegos = juegos
+          ) 
+        );
+    }
+
   }
 }
